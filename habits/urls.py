d6e7e6df0c1863a0845fb34ecfaddfc1a95a8809
Urffin-Juss <im@maxsuns.ru>
2026-02-06 08:24:35 +0300

    add urls

--- habits/urls.py
+++ habits/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from habits.views import HabitsViewSet
+from habits.views import HabitsViewSet, PublicHabitViewSet
 
 app_name = 'habits'
 router = DefaultRouter()
 router.register(r'habits', HabitsViewSet, basename='habits')
+router.register(r'public-habits', PublicHabitViewSet, basename='public-habits')
 
-urlpatterns = [
-
-
-
-]+router.urls
+urlpatterns = router.urls
