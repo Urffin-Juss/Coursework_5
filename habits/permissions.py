4aa4bfac38edebb059952dbfc793528caf3f62a0
Urffin-Juss <im@maxsuns.ru>
2026-02-06 17:27:05 +0300

    fix some

--- habits/permissions.py
+++ habits/permissions.py
@@ -2,5 +2,5 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        def has_object_permission(self, request, view, obj):
-            return obj.user == request.user
+
+        return obj.user == request.user
