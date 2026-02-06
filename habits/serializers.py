4628a459ca62c4d5aeeedd8579f97cfae850586d
Urffin-Juss <im@maxsuns.ru>
2026-02-06 08:17:10 +0300

    rewrite serial

--- habits/serializers.py
+++ habits/serializers.py
@@ -3,33 +3,37 @@ from rest_framework import serializers, permissions
 from habits.models import Habit
 
 
-class RelatedHabitsSerializer(serializers.ModelSerializer):
-
-
-    class Meta:
-        model = Habit
-        fields = '__all__'
-
-
-    @staticmethod
-    def validate_fields(data):
-        is_pleasant = data.get('is_pleasant')
-        reward= data.get('reward')
-
-        if is_pleasant  is None and reward is None:
-            raise serializers.ValidationError('You must select a reward or is_pleasant')
-
-        elif is_pleasant and reward:
-            raise serializers.ValidationError('You must select a reward or is_pleasant')
-
-
-
-
 class HabitsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Habit
         fields = '__all__'
+        read_only_fields = ('user')
+
+        def __init__(self):
+            self.instance = None
+
+        def validate(self, attrs):
+            is_pleasant = attrs.get('is_pleasant', getattr(self.instance, 'is_pleasant', False))
+            reward = attrs.get('reward', getattr(self.instance, 'reward', None))
+            related_habits = attrs.get('related_habits', getattr(self.instance, 'related_habits', None))
+            time_to_complete = attrs.get('time_to_complete', getattr(self.instance, 'time_to_complete', None))
+            periodicity = attrs.get('periodicity', getattr(self.instance, 'periodicity', None))
+
+            if reward and related_habits:
+                raise serializers.ValidationError ("You cannot set both reward and related_habits")
+
+            if time_to_complete is not None and periodicity > 120:
+                raise serializers.ValidationError ("Time to complete must be less than 120 minutes")
+
+            if not(1 <= periodicity <= 7):
+                raise serializers.ValidationError ("Periodicity must be between 1 and 7")
+
+            if is_pleasant and (reward or related_habits):
+                raise serializers.ValidationError ("Pleasant and reward cannot be set at the same time")
+
+            if related_habits and not related_habits.is_pleasant:
+                raise serializers.ValidationError ("Related habits need to be a pleasant")
 
 
 
