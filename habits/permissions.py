9750e07d87edf9816ed81337b6f0084492c65c82
Urffin-Juss <im@maxsuns.ru>
2026-02-06 17:35:21 +0300

    new perm class

--- habits/permissions.py
+++ habits/permissions.py
@@ -4,3 +4,12 @@ class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
 
         return obj.user == request.user
+
+
+
+class IsPublicOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return obj.is_public() or obj.user == request.user
+        return obj.user == request.user
+
