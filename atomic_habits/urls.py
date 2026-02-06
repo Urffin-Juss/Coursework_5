60a3e3cf4c02636398a6a497a14afef9319c2244
Urffin-Juss <im@maxsuns.ru>
2026-02-06 13:22:33 +0300

    checkpoint

--- atomic_habits/urls.py
+++ atomic_habits/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('habits/', include('habits.urls')),
-    path('users/', include('users.urls')),
+    #path('users/', include('users.urls')),
 ]
