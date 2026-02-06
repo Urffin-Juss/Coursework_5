3ca5ed3aa4aa09d7d18bab346ccf09a41cc3d599
Urffin-Juss <im@maxsuns.ru>
2026-02-06 17:53:34 +0300

    UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from users.serializer import UserSerializer
+from models import User
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
 
 # Create your views here.
