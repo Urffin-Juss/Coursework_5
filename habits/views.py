839021807531cdd0768b5a442056d4419f5535a3
Urffin-Juss <im@maxsuns.ru>
2026-02-08 08:56:32 +0300

    add doc

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ReadOnlyModelViewSet
@@ -12,6 +14,85 @@ class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitsSerializer
     permission_classes = [IsAuthenticated, IsOwner]
     pagination_class = HabitPagination
+
+    @swagger_auto_schema(
+        operation_description="Создание",
+        operation_summary="habit_create",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "habit_name": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Название привычки"
+                ),
+                "owner": openapi.Schema(
+                    type=openapi.TYPE_INTEGER, description="Создатель привычки"
+                ),
+                "place": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Место выполнения привычки"
+                ),
+                "time": openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    description="Время для выполнения привычки. Не может превышать 120 секунд.",
+                ),
+                "action": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Действие"
+                ),
+                "is_pleasant": openapi.Schema(
+                    type=openapi.TYPE_BOOLEAN,
+                    description="Признак полезной привычки. Не может указываться у привычек с выбранным reward и \
+                        если выбрано related_habit",
+                ),
+                "related_habit": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    description="Связанная привычка. Должна быть привычка с признаком is_pleasant. Не может быть \
+                        указана у привычки с признаком is_pleasant и если выбрано reward",
+                ),
+                "periodicity": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    description="Периодичность выполнения. Должна быть в диапазоне от 1 до 7 дней.",
+                ),
+                "reward": openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    description="Вознаграждение. Не может быть указана у привычки с признаком is_pleasant и если \
+                        выбрана related_habit",
+                ),
+                "time_to_complete": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    description="Время на выполнение привычки",
+                ),
+                "is_public": openapi.Schema(
+                    type=openapi.TYPE_BOOLEAN, description="Признак публичной привычки"
+                ),
+            },
+            required=["habit_name", "place", "action", "time_to_complete"],
+        ),
+        responses={
+            "201": openapi.Response(
+                description="Объект создан",
+                schema=HabitSerializer,
+                examples={
+                    "application/json": {
+                        "id": 1,
+                        "owner": 3,
+                        "habit_name": "Отжимания",
+                        "place": "В любом месте",
+                        "time": "18:32:00",
+                        "action": "Отжимания",
+                        "is_pleasant": False,
+                        "periodicity": 1,
+                        "reward": None,
+                        "is_public": False,
+                        "time_to_complete": 120,
+                        "created_at": "2026-02-07T18:14:38.910300+03:00",
+                    }
+                },
+            ),
+            "400": "Ошибки валидации",
+            "401": "Не авторизован",
+        },
+        tags=["habit"],
+    )
+
 
     def get_queryset(self):
         return Habit.objects.filter(user=self.request.user)
--- users/views.py
+++ users/views.py
@@ -1,41 +1,40 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from users.serializer import UserSerializer
-from .models import  User
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializer import UserSerializer
 
 
+class RegisterView(APIView):
+    """Представление для регистрации"""
 
-class UserCreateAPIView(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
-
-class UserListAPIView(ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class UserRetrieveAPIView(RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class UserUpdateAPIView(UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class UserDestroyAPIView(DestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-
-
-
-# Create your views here.
+    @swagger_auto_schema(
+        operation_description="Регистрация",
+        operation_summary="Регистрация нового пользователя",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description="Почта"),
+                "password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Пароль"
+                ),
+                "confirm_password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="Подтверждение пароля"
+                ),
+            },
+            required=["email", "password", "confirm_password"],
+        ),
+        responses={
+            201: openapi.Response(description="1"),
+            400: "Ошибка валидации данных",
+        },
+        tags=["register"],
+    )
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
